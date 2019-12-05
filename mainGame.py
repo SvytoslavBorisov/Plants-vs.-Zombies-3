265a08814e582dcafa6decce770f39ac1945b2c1
Svytoslav <sv_borisov03@rambler.ru>
2019-12-05 17:20:57 +0400

    Sprites

--- mainGame.py
+++ mainGame.py
@@ -1,2 +1,23 @@
-for i in range(int(input())):
-  print(i)
+import pygame
+
+
+pygame.init()
+size = width, height = 700, 600
+screen = pygame.display.set_mode(size)
+screen.fill((0, 0, 0))
+GREEN = pygame.Color(0, 255, 0)
+YELLOW = pygame.Color(0, 255, 255)
+BLACK = pygame.Color(0, 0, 0)
+
+sBackGround = pygame.image.load('Graphics/SelectorScreen_BG_Right.jpg').convert()
+
+screen.blit(sBackGround, (0, 0))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+
+pygame.quit()
