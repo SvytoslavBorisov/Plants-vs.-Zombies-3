e820e32675f6eccfc2d65d8578c5056bb7b112a6
SvytoslavYandex <sv_borisov03@rambler.ru>
2019-12-05 21:43:40 +0400

    First

---
+++ mainGame.py
@@ -0,0 +1,115 @@
+import pygame
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen.fill((0, 0, 0))
+WHITE = pygame.Color(255, 255, 255)
+GREEN = pygame.Color(0, 255, 0)
+WallNut_body = pygame.image.load('Graphics/Wallnut_body.png')
+Card_GatlingPea = pygame.transform.scale(pygame.image.load('Graphics/Card_GatlingPea.png'), (100, 100))
+
+
+class BoardOfCards:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [['black'] * height for _ in range(width)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+        self.data = []
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self):
+        for g in range(self.width):
+            for i in range(self.height):
+                pygame.draw.rect(screen, WHITE, (g * self.cell_size + self.left,
+                                                 i * self.cell_size + self.top, self.cell_size,
+                                                 self.cell_size))
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def on_click(self, cell):
+        if cell:
+            screen.blit(Card_GatlingPea, (self.cell_size * cell[0] + self.left, self.cell_size * cell[1] + self.top, self.cell_size, self.cell_size))
+
+    def get_cell(self, mouse_pos):
+        self.data.clear()
+        if self.left < mouse_pos[0] < self.left + self.width * self.cell_size \
+                and self.top < mouse_pos[1] < self.top + self.height * self.cell_size:
+            self.data.append((mouse_pos[0] - self.left) // self.cell_size)
+            self.data.append((mouse_pos[1] - self.top) // self.cell_size)
+            return self.data
+        else:
+            return None
+
+
+class BoardOfGame:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [['green'] * height for _ in range(width)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+        self.data = []
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self):
+        for g in range(self.width):
+            for i in range(self.height):
+                pygame.draw.rect(screen, GREEN, (g * self.cell_size + self.left,
+                                                 i * self.cell_size + self.top, self.cell_size,
+                                                 self.cell_size))
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def on_click(self, cell):
+        if cell:
+            screen.blit(WallNut_body, (self.cell_size * cell[0] + self.left,
+                                       self.cell_size * cell[1] + self.top, self.cell_size, self.cell_size))
+
+    def get_cell(self, mouse_pos):
+        self.data.clear()
+        if self.left < mouse_pos[0] < self.left + self.width * self.cell_size \
+                and self.top < mouse_pos[1] < self.top + self.height * self.cell_size:
+            self.data.append((mouse_pos[0] - self.left) // self.cell_size)
+            self.data.append((mouse_pos[1] - self.top) // self.cell_size)
+            return self.data
+        else:
+            return None
+
+
+boardGame = BoardOfGame(9, 5)
+boardGame.set_view(357, 104, 100)
+boardGame.render()
+
+boardCards = BoardOfCards(1, 5)
+boardCards.set_view(5, 5, 100)
+boardCards.render()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            boardCards.get_click(event.pos)
+            boardGame.get_click(event.pos)
+
+    pygame.display.flip()
+
+pygame.quit()
