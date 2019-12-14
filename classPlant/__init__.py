0f6dcff35d2cf726703040a91b3225299391b817
SvytoslavYandex <sv_borisov03@rambler.ru>
2019-12-14 20:45:53 +0400

    Update(no lag)

--- classPlant/__init__.py
+++ classPlant/__init__.py
@@ -16,19 +16,61 @@ def load_image(name, size, colorkey=None):
 
 class Plant(pygame.sprite.Sprite):
 
-    def __init__(self, fileOfSprite, count, size):
+    def __init__(self, sprites):
         super().__init__()
         self.images_list = []
         self.rects = []
         self.cur_frame = 0
-        if count != 0:
-            for i in range(count):
-                self.images_list.append(load_image(f'{fileOfSprite}/{i}.png', size))
-                self.rects.append(self.images_list[i].get_rect())
-        else:
-            self.images_list.append(load_image(f'{fileOfSprite}.png', size))
-            self.rects.append(self.images_list[0].get_rect())
+        for i in range(len(sprites)):
+            self.images_list.append(sprites[i])
+            self.rects.append(self.images_list[i].get_rect())
 
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.images_list)
         self.image = self.images_list[self.cur_frame]
+
+
+class GatlingPea(Plant):
+
+    def __init__(self, spritesNormal, spritesShoot):
+        self.images_listNormal = []
+        self.rectsNormal = []
+
+        self.images_listShoot = []
+        self.rectsShoot = []
+
+        self.cur_frame = 0
+        self.glb_cur_frame = 0
+
+        for i in range(len(spritesNormal)):
+            self.images_listNormal.append(spritesNormal[i])
+            self.rectsNormal.append(self.images_listNormal[i].get_rect())
+
+        for i in range(len(spritesShoot)):
+            self.images_listShoot.append(spritesShoot[i])
+            self.rectsShoot.append(self.images_listShoot[i].get_rect())
+
+        self.glb_len = len(self.images_listNormal)
+
+    def update(self):
+
+        if self.glb_cur_frame == 0:
+            self.cur_frame = (self.cur_frame + 1) % self.glb_len
+            if self.cur_frame == 0:
+                self.glb_len = len(self.images_listShoot)
+                self.image = self.images_listShoot[self.cur_frame]
+                self.glb_cur_frame = 1
+            else:
+                self.glb_len = len(self.images_listNormal)
+                self.image = self.images_listNormal[self.cur_frame]
+                self.glb_cur_frame = 0
+        else:
+            self.cur_frame = (self.cur_frame + 1) % self.glb_len
+            if self.cur_frame == 0:
+                self.glb_len = len(self.images_listNormal)
+                self.image = self.images_listNormal[self.cur_frame]
+                self.glb_cur_frame = 0
+            else:
+                self.glb_len = len(self.images_listShoot)
+                self.image = self.images_listShoot[self.cur_frame]
+                self.glb_cur_frame = 1
