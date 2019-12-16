abe2188e9e089f0879014f83562dc6ef820fd62d
WwWSrJSilverWwW <57539547+WwWSrJSilverWwW@users.noreply.github.com>
2019-12-16 14:10:29 +0400

    Add files via upload

---
+++ allConstants.py
@@ -0,0 +1,27 @@
+import pygame
+
+SIZE1 = WIDTH1, HEIGHT1 = 700, 600
+SIZE2 = WIDTH2, HEIGHT2 = 1026, 600
+SIZE_OF_PLANT = (80, 80)
+
+FPS = 20
+
+RED = pygame.Color("red")
+GREEN = pygame.Color("green")
+BLUE = pygame.Color("blue")
+YELLOW = pygame.Color("yellow")
+BLACK = pygame.Color("black")
+
+FIELD_WIDTH = 9
+FIELD_HEIGHT = 5
+FIELD_CELL_WIDTH = 82
+FIELD_CELL_HEIGHT = 99
+FIELD_LEFT = 245
+FIELD_TOP = 80
+
+PANEL_WIDTH = 6
+PANEL_CELL_WIDTH = 62
+PANEL_CELL_HEIGHT = 85
+PANEL_LEFT = 300
+PANEL_TOP = 5
+PANEL_STEP = 10
