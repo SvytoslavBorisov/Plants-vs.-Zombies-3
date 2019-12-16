ad4f1278850975cee84b6ba16244826243cd9024
WwWSrJSilverWwW <57539547+WwWSrJSilverWwW@users.noreply.github.com>
2019-12-16 14:09:56 +0400

    Add files via upload

---
+++ classPanel.py
@@ -0,0 +1,53 @@
+import pygame
+
+
+class Panel:
+    def __init__(self, width, cell_width, cell_height, left, top, step, screen):
+        self.width = width
+        self.height = 1
+        self.cell_width = cell_width
+        self.cell_height = cell_height
+        self.left = left
+        self.top = top
+        self.screen = screen
+        self.step = step
+        self.board = [[]]
+        for i in range(self.width):
+            for j in range(self.height):
+                self.board[i].append('')
+            self.board.append([])
+        self.data = []
+
+    def render(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                pygame.draw.rect(self.screen, pygame.Color("white"),
+                                 pygame.Rect((self.left + j * self.cell_width + self.step * j,
+                                              self.top + i * self.cell_height),
+                                             (self.cell_width, self.cell_height)), 5)
+
+                if self.board[j][i] != '':
+                    self.board[j][i].update()
+                    self.screen.blit(self.board[j][i].image, (self.cell_width * j + self.left,
+                                               self.cell_height * i + self.top,
+                                               self.cell_width,
+                                               self.cell_height))
+
+    def get_click(self, mouse_pos, plant):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell, plant)
+
+    def on_click(self, cell, plant):
+        if self.board[cell[0]][cell[1]] == '':
+            self.board[cell[0]][cell[1]] = plant
+
+    def get_cell(self, mouse_pos):
+        self.data.clear()
+        if self.left < mouse_pos[0] < self.left + self.width * self.cell_width \
+                and self.top < mouse_pos[1] < self.top + self.height * self.cell_height:
+            self.data.append((mouse_pos[0] - self.left) // self.cell_width)
+            self.data.append((mouse_pos[1] - self.top) // self.cell_height)
+            return self.data
+        else:
+            return None
--- mainGame.py
+++ mainGame.py
@@ -1,7 +1,9 @@
 import pygame
 from classPlant import Plant, load_image, GatlingPea, Sunrise
+from classPanel import Panel
+from classField import Field
+from allConstants import *
 import sys
-FPS = 50
 
 
 def terminate():
@@ -35,107 +37,42 @@ def start_screen():
         clock.tick(FPS)
 
 
-class Field:
-    def __init__(self, width, height, cell_width, cell_height, left, top):
-        self.width = width
-        self.height = height
-        self.cell_width = cell_width
-        self.cell_height = cell_height
-        self.left = left
-        self.top = top
-        self.board = [[]]
-        for i in range(self.width):
-            for j in range(self.height):
-                self.board[i].append('')
-            self.board.append([])
-        self.data = []
-
-    def render(self):
-        global screen
-        for i in range(self.height):
-            for j in range(self.width):
-                #pygame.draw.rect(screen, pygame.Color("white"),
-                #                 pygame.Rect((self.left + j * self.cell_width,
-                #                              self.top + i * self.cell_height),
-                #                             (self.cell_width, self.cell_height)), 1)
-
-                if self.board[j][i] != '':
-                    temp = self.board[j][i].update()
-                    if temp:
-                        screen.blit(temp, (self.cell_width * j + self.left + 50,
-                                               self.cell_height * i + self.top + 50,
-                                               self.cell_width,
-                                               self.cell_height))
-                    screen.blit(self.board[j][i].image, (self.cell_width * j + self.left,
-                                               self.cell_height * i + self.top,
-                                               self.cell_width,
-                                               self.cell_height))
-
-    def get_click(self, mouse_pos, plant):
-        cell = self.get_cell(mouse_pos)
-        if cell:
-            self.on_click(cell, plant)
-
-    def on_click(self, cell, plant):
-        if self.board[cell[0]][cell[1]] == '':
-            self.board[cell[0]][cell[1]] = plant
-
-    def get_cell(self, mouse_pos):
-        self.data.clear()
-        if self.left < mouse_pos[0] < self.left + self.width * self.cell_width \
-                and self.top < mouse_pos[1] < self.top + self.height * self.cell_height:
-            self.data.append((mouse_pos[0] - self.left) // self.cell_width)
-            self.data.append((mouse_pos[1] - self.top) // self.cell_height)
-            return self.data
-        else:
-            return None
-
-
 pygame.init()
-size = width, height = 700, 600
 #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(SIZE1)
 clock = pygame.time.Clock()
 
 start_screen()
 
-screen.fill((0, 0, 0))
-GREEN = pygame.Color(0, 255, 0)
-YELLOW = pygame.Color(0, 255, 255)
-BLACK = pygame.Color(0, 0, 0)
-
-plants = {'wallNut': [load_image(f'Graphics/animationGatlingPea/{i}.png', (80, 80)) for i in range(23)],
-          'gatlingPea': [load_image(f'Graphics/animationGatlingPea/{i}.png', (80, 80)) for i in range(23)],
-          'gatlingPeaShoot': [load_image(f'Graphics/animationGatlingPeaShoot/{i}.png', (80, 80)) for i in range(15)],
-          'sunrise': [load_image(f'Graphics/animationSunrise/{i}.png', (80, 80)) for i in range(16)],
-          'sunriseGiveSun': [load_image(f'Graphics/animationSunriseGiveSun/{i}.png', (80, 80)) for i in range(14)],
-          'pea': load_image(f'Graphics/other/pea.png', (20, 20))}
+plants = {'wallNut': [load_image(f'Graphics/animationGatlingPea/{i}.png', SIZE_OF_PLANT) for i in range(23)],
+          'gatlingPea': [load_image(f'Graphics/animationGatlingPea/{i}.png', SIZE_OF_PLANT) for i in range(23)],
+          'gatlingPeaShoot': [load_image(f'Graphics/animationGatlingPeaShoot/{i}.png', SIZE_OF_PLANT) for i in range(15)],
+          'sunrise': [load_image(f'Graphics/animationSunrise/{i}.png', SIZE_OF_PLANT) for i in range(15)]}
 
 sBackGround = pygame.image.load('Graphics/other/Frontyard.jpg').convert()
-size = WIDTH, HEIGHT = 1026, 600
 
-split_point_x = 1026
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(SIZE2)
 
 screen.blit(sBackGround, (0, 0))
-field = Field(9, 5, 82, 99, 245, 80)
+field = Field(FIELD_WIDTH, FIELD_HEIGHT, FIELD_CELL_WIDTH, FIELD_CELL_HEIGHT, FIELD_LEFT, FIELD_TOP, screen)
+panel = Panel(PANEL_WIDTH, PANEL_CELL_WIDTH, PANEL_CELL_HEIGHT, PANEL_LEFT, PANEL_TOP, PANEL_STEP, screen)
 
 running = True
-while running:
-    clock.tick(25)
-    screen.fill(BLACK)
-    screen.blit(sBackGround, (0, 0))
 
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                field.get_click(event.pos, GatlingPea(plants['gatlingPea'], plants['gatlingPeaShoot'], plants['pea']))
+                field.get_click(event.pos, GatlingPea(plants['gatlingPea'], plants['gatlingPeaShoot']))
             else:
-                field.get_click(event.pos, Sunrise(plants['sunrise'], plants['sunriseGiveSun']))
+                field.get_click(event.pos, Sunrise(plants['sunrise']))
 
+    screen.blit(sBackGround, (0, 0))
+    panel.render()
     field.render()
     pygame.display.flip()
+    clock.tick(FPS)
 
 pygame.quit()
