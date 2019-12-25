e11d8448877c75d037a1c64fc51262b3795bdd61
Svytoslav <sv_borisov03@rambler.ru>
2019-12-25 11:52:29 +0400

    Svytoslav разделение на экраны

--- mainGame.py
+++ mainGame.py
@@ -2,7 +2,7 @@ import pygame
 from classPlant import *
 from classPanel import Panel
 from classField import Field
-from classZombie import konusZombie
+from zombies import classZombie
 from allConstants import *
 import sys
 
@@ -33,7 +33,34 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return shields['game']()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def game():
+    suns = 0
+    zs = pygame.sprite.Group()
+    screen.blit(sBackGround, (0, 0))
+    field = Field(FIELD_WIDTH, FIELD_HEIGHT, FIELD_CELL_WIDTH, FIELD_CELL_HEIGHT, FIELD_LEFT, FIELD_TOP, screen)
+    panel = Panel(PANEL_WIDTH, PANEL_CELL_WIDTH, PANEL_CELL_HEIGHT, PANEL_LEFT, PANEL_TOP, PANEL_STEP, screen)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if field.get_click(event.pos, panel.checkPlant):
+                    panel.checkPlant = ''
+                panel.get_click(event.pos)
+
+        if random.choice([0] * 44 + [1]):
+            zs.add(classZombie.konusZombie(random.randint(0, 4), 100))
+
+        screen.blit(sBackGround, (0, 0))
+        panel.render()
+        field.render()
+        zs.update()
+        zs.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -43,40 +70,11 @@ pygame.display.set_caption('Plants VS Zombies 4')
 pygame.display.set_icon(pygame.image.load('Graphics/other/icon.png'))
 #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen = pygame.display.set_mode(SIZE2)
-
 clock = pygame.time.Clock()
 
-start_screen()
+shields = {'start_screen': start_screen,
+           'game': game}
+while True:
+    temp = shields['start_screen']()
 
-sBackGround = pygame.image.load('Graphics/other/Frontyard.jpg').convert()
 
-screen.blit(sBackGround, (0, 0))
-field = Field(FIELD_WIDTH, FIELD_HEIGHT, FIELD_CELL_WIDTH, FIELD_CELL_HEIGHT, FIELD_LEFT, FIELD_TOP, screen)
-panel = Panel(PANEL_WIDTH, PANEL_CELL_WIDTH, PANEL_CELL_HEIGHT, PANEL_LEFT, PANEL_TOP, PANEL_STEP, screen)
-
-running = True
-
-suns = 0
-zs = pygame.sprite.Group()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if field.get_click(event.pos, panel.checkPlant):
-                panel.checkPlant = ''
-            panel.get_click(event.pos)
-            
-    if random.choice([0] * 44 + [1]):
-        zs.add(konusZombie(random.randint(0, 4), 100))
-        
-    screen.blit(sBackGround, (0, 0))
-    panel.render()
-    field.render()
-    zs.update()
-    zs.draw(screen)
-    pygame.display.flip()
-    clock.tick(FPS)
-    
-pygame.quit()
